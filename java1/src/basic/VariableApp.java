5f8aeefc291293c67a1ccf72560b84ba97254738
itwill <itwill@DESKTOP-TDFT6LO>
2023-10-20 17:25:36 +0900

    Day2

--- java1/src/basic/VariableApp.java
+++ java1/src/basic/VariableApp.java
@@ -24,9 +24,22 @@ public class VariableApp {
 		//" " - 문자가 없는 비어있는 문자열(NullString)
 		System.out.println(""+2+0+2+3+"년은 토키띠입니다.");//잘못된 결과 발생 - 실행 오류
 		System.out.println("==================================================");
-	
-	
-	
-
+		//동일한 자료형의 변수는 , 기호를 사용하여 나열 선언 가능
+		int num1=100, num2=200;
+		System.out.println("연산결과 = "+num1+num2);
+		System.out.println("연산결과 = "+(num1+num2));
+		System.out.println("연산결과 = "+(num1*num2));
+		System.out.println("==================================================");
+		int kor=88, eng=90;
+		//int tot=88-90;//리터럴 사용보다는 변수 사용하기!
+		int tot=kor+eng;//변수값으로 연산 처리하고 결과값을 변수에 저장 - 처리 명령
+		System.out.println("점수 합계 = "+tot);
+		System.out.println("==================================================");
+		//변수에 값이 저장되어 있지 않은 상태에서 변수를 사용할 경우 에러 발생
+		int count=10;
+		System.out.println("count ="+count);
+		System.out.println("==================================================");
+		
+		
 	}
 }
