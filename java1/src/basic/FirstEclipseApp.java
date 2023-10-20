32268da4a96bc6c8d495f58c2debb20c9cd5e60c
itwill <itwill@DESKTOP-TDFT6LO>
2023-10-20 12:48:52 +0900

    2Day

---
+++ java1/src/basic/FirstEclipseApp.java
@@ -0,0 +1,9 @@
+package basic;
+
+
+public class FirstEclipseApp {
+	public static void main(String[] args) {
+		System.out.print("이클립스에서 처음 만든 프로그램입니다.");//출력매소드 호출
+		System.out.println("재미있게 만들어 봐요.");/* 출력매소드 호출 */
+	}
+}
