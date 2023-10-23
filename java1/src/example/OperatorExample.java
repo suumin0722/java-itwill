3a10a4d01e9795bf812b4840ac47ed1b786af4bd
itwill <itwill@DESKTOP-TDFT6LO>
2023-10-23 18:05:00 +0900

    Day3

---
+++ java1/src/example/OperatorExample.java
@@ -0,0 +1,9 @@
+package example;
+
+public class OperatorExample {
+	public static void main(String[] args) {
+		
+		//1. %연산자
+		//2. 삼항연산자
+	}
+}
---
+++ java1/src/example/VariableExample.java
@@ -0,0 +1,29 @@
+package example;
+
+public class VariableExample {
+	public static void main(String[] args) {
+		//가로의 길이가 7이고 세로의 길이가 10인 사각형의 넓이를 계산하여 출력하세요.
+		int garo=7, sero=10;
+		int nulbe=garo*sero;
+		System.out.println("사각형의 넓이 = "+nulbe);
+		
+		
+		System.out.println("====================================================");
+		//높이가 9이고 밑변의 길이가 7인 삼각형의 넓이를 계산하여 출력하세요.
+
+	
+
+		System.out.println("====================================================");
+		//10명의 전체 몸무게가 759kg인 경우 평균 몸무게를 계산하여 출력하세요.
+
+		
+		System.out.println("====================================================");
+		//
+		
+	
+		System.out.println("====================================================");
+	
+	
+	
+	}
+}
