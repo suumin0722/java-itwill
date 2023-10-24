1c592bca5f655c7a2a40acf710b31344c9449409
itwill <itwill@DESKTOP-TDFT6LO>
2023-10-24 19:18:20 +0900

    Day4

--- java1/src/example/IfExample.java
+++ java1/src/example/IfExample.java
@@ -11,6 +11,11 @@ public class IfExample {
 		//변수에 저장된 정수값이 4의 배수인지 아닌지를 구분하여 출력하세요.
 		int num=345644;
 		
+		if(num % 4 == 0) {
+			System.out.println(num+" = 4의 배수");
+		}	else {
+			System.out.println(num+"은 4의 배수가 아님");
+		}
 		
 		System.out.println("============================================================");
 		//이름이 [홍길동]인 학생이 국어점수 89점, 영어점수 93점, 수학점수 95점을 받은 경우
@@ -20,7 +25,30 @@ public class IfExample {
 		// => 평균을 이용한 학점 계산 : 100~90:A, 89~80:B, 79~70:C, 69~60:D, 59~0:F
 		// => 평균은 소숫점 두자리까지만 출력하고 나머지는 절삭 처리 하세요.
 		
+		String name ="홍길동";
+		int kor=89, eng=93, mat=95;
+		
+		int tot=kor+eng+mat;
+		double ave=tot/3.;
+		
+		if(kor>=0 && kor<=100 && eng>=0 && eng<=100 && mat>=0 && mat <=100) {
+			String grade;
+			
+			switch((int)ave/10) {
+			case 10:
+			case 9: grade="A"; break;
+			case 8: grade="B"; break;
+			case 7: grade="C"; break;
+			case 6: grade="D"; break;
+			default: grade="F";
+			}
+			
+			System.out.println(((int)(ave*100)/100.)+"학점");
+		} else {
+			System.exit(0);
+	
 		System.out.println("============================================================");
+		}
 	}
 }
 
