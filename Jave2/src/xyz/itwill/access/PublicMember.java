a574eb277478ff78bcfc4e839ed5d337a8cd52c8
itwill <suumin0722@naver.com>
2023-11-06 12:06:52 +0900

    Day13

---
+++ Jave2/src/xyz/itwill/access/PublicMember.java
@@ -0,0 +1,21 @@
+package xyz.itwill.access;
+
+//public: 클래스, 필드, 생성자, 메소드 작성시 사용 가능한 접근 제한자
+// =>클래스, 필드, 생성자, 메소드를 같은 패키지 및 다른 패키지의 외부 클래스에서 사용
+//가능하도록 설정하는 접근 제한자
+
+//소스파일에는 다수의 Java 자료형(클래스, 인터페이스, 열거형) 선언 가능 - 비권장
+// =>소스파일에는 public 제한자로 선언된 클래스를 하나만 작성 가능
+// =>public 제한자를 사용한 Java 자료형의 이름으로 소스파일명을 저장
+
+public class PublicMember {
+	public int number;
+	
+	public void display() {
+		System.out.println("number = "+number);
+		
+	}
+}
+
+
+	
