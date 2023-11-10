9a4cacda70c63930531307794687fffd4feebe6b
itwill <suumin0722@naver.com>
2023-11-10 18:09:09 +0900

    Day17

---
+++ java3/src/xyz/itwill/lang/PersonNumberApp.java
@@ -0,0 +1,60 @@
+package xyz.itwill.lang;
+
+import java.util.Scanner;
+//키보드로 주민번호를 입력받아 입력받은 주민번호를 사용하여 생년월일과 성별을 계산하여 출력하는 프로그램 작성
+//=> 주민번호는 7번째 자리에 반드시 [-] 문자가 존재하고 14자리로 입력받도록 처리
+//=> 비정상적인 주민번호가 입력된 경우 에러 메세지를 출력하고 재입력 되도록 처리
+//ex) 주민번호[ex.901225-1234567] >> 000101-4567890
+// [결과]생년월일 = 2000년 01월 01일, 성별 = 여자 
+
+public class PersonNumberApp {
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		
+		String idNumber;//주민번호를 저장하기 위한 변수
+		//사용자로부터 입력받은 주민번호를 검증하기 위한 반복문
+		//=>정상적인 주민번호를 입력받은 경우 반복문 종료
+		while(true) {
+			System.out.print("주민등록번호 입력 >> ");
+			//키보드로 입력받은 문자열에서 앞뒤에 존재하는 불필요한 공백을 제거하여 변수에 저장
+			idNumber=scanner.nextLine().trim();
+			
+			
+			//if(idNumber.length()==14 && idNumber.charAt(6)=='-') break;
+			if(idNumber.length()==14 && idNumber.indexOf("-")==6) break;
+			
+			System.out.println("[에러]형식에 맞게 주민번호를 다시 입력해주세요.");
+		}
+		scanner.close();
+		
+		//입력된 주민번호에서 8번째 위치의 문자 하나를 문자열로 분리하여 저장
+		String separation=idNumber.substring(7,8);
+		
+		//생년원일을 저장하기 위한 변수
+		String birthday="";
+		
+		//separation 변수에 저장된 문자열이 [1] 또는 [2]인 경우 - 20세기(19XX)에 태어난 경우
+		//separation 변수에 저장된 문자열이 [3] 또는 [4]인 경우 - 21세기(20XX)에 태어난 경우
+		if(separation.equals("1") || separation.equals("2")) {
+			birthday+="19";//문자열을 추가하여 변수에 저장
+		} else if (separation.equals("3") || separation.equals("4")) {
+			birthday+="20";
+		}
+		
+		//입력된 주민번호에서 생년원일을 잘라내어 변수에 추가하여 저장
+		birthday+=idNumber.substring(0,2)+"년 ";
+		birthday+=idNumber.substring(2,4)+"월 ";
+		birthday+=idNumber.substring(4,6)+"일";
+		
+		String gender="";//성별을 저장하기 위한 변수
+		if(separation.equals("1") || separation.equals("3")) {
+			gender="남자";
+		} else if (separation.equals("2") || separation.equals("4")) {
+			gender="여자";
+		}
+		
+		System.out.println("생년원일 = "+birthday+", 성별 = "+gender);
+	}
+}
+
+
