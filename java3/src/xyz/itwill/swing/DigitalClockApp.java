e7fea4fbc933ef4490a6e08b4e4ccfca90d5e1ba
itwill <suumin0722@naver.com>
2023-11-17 17:06:39 +0900

    Day22

--- java3/src/xyz/itwill/swing/DigitalClockApp.java
+++ java3/src/xyz/itwill/swing/DigitalClockApp.java
@@ -2,11 +2,15 @@ package xyz.itwill.swing;
 
 import java.awt.BorderLayout;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 //플랫폼의 현재 날짜와 시간을 1초마다 제공받아 출력하는 GUI 프로그램 작성
 // => 새로운 스레드를 생성하여 플랫폼의 현재 날짜와 시간을 1초마다 제공받아 컴퍼넌트 변경 처리
@@ -14,11 +18,17 @@ public class DigitalClockApp extends JFrame {
 	private static final long serialVersionUID = 1L;
 	
 	JLabel clockLabel;
+	JButton startButton, stopButton;
 	
+	//플랫폼의 현재 날짜와 시간 정보를 제공받아 출력하는 스레드의 실행상태를 저장하기 위한 필드
+	// => false: 스레드 일시중지 상태, true: 스레드 실행 상태
+	private boolean isRun;
 	
 	public DigitalClockApp(String title) {
 		super(title);
 		
+		isRun=true;
+				
 		/*
 		Date now=new Date();
 		SimpleDateFormat df=new SimpleDateFormat("yyyy년 MM월 dd일 HH시 mm분 ss초");
@@ -28,6 +38,20 @@ public class DigitalClockApp extends JFrame {
 		//JLabel clockLabel=new JLabel("2023년 11월 17일 13시 03분 20초", JLabel.CENTER);
 		clockLabel=new JLabel("", JLabel.CENTER);
 		clockLabel.setFont(new Font("굴림체", Font.BOLD, 30));
+		
+		
+		startButton=new JButton("다시 실행");
+		stopButton=new JButton("일시 중지");
+		startButton.setFont(new Font("굴림체", Font.BOLD, 20));
+		stopButton.setFont(new Font("굴림체", Font.BOLD, 20));
+		
+		startButton.setEnabled(false);
+		
+		JPanel panel=new JPanel();
+		panel.add(startButton);
+		panel.add(stopButton);
+		
+		
 
 		//스레드 클래스로 객체를 생성하여 start() 메소드를 호출해 새로운 스레드를 만들어
 		//run() 메소드의 명령 실행
@@ -61,10 +85,12 @@ public class DigitalClockApp extends JFrame {
 				SimpleDateFormat df=new SimpleDateFormat("yyyy년 MM월 dd일 HH시 mm분 ss초");
 			
 				while(true) {
-					Date now=new Date();
-					String printDate=df.format(now);
-					clockLabel.setText(printDate);//컴퍼넌트의 문자열 변경
-
+					//isRun 필드값이 [true]인 경우에만 플랫폼의 현재 날짜와 시간 정보를 제공받아
+					//컴퍼넌트의 문자열 변경 처리
+					if(isRun) {
+						clockLabel.setText(df.format(new Date()));
+					}
+					
 					try {
 						Thread.sleep(1000);
 					} catch (InterruptedException e) {
@@ -73,8 +99,12 @@ public class DigitalClockApp extends JFrame {
 				}
 		}).start();	
 		
+		//JVM(Java Virtual Machine)은 이벤트를 처리하기 위한 Event-Queue 스레드 자동 생성
+		startButton.addActionListener(new ClockButtonEventHandle());
+		stopButton.addActionListener(new ClockButtonEventHandle());
 		
 		getContentPane().add(clockLabel, BorderLayout.CENTER);
+		getContentPane().add(panel, BorderLayout.SOUTH);
 		
 		
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -111,6 +141,29 @@ public class DigitalClockApp extends JFrame {
 		}
 	}
 	*/
+	
+	//JButton 컴퍼넌트를 누른 경우 이벤트 처리를 위해 작성한 클래스
+	// => 버튼을 누른 경우 플랫폼의 현재 날짜와 시간 정보를 제공받아 출력하는 스레드를 일시 중지하거나
+	//재실행되도록 설정
+	public class ClockButtonEventHandle implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			Object eventSource=e.getSource();
+			
+			if(eventSource == startButton) {
+				startButton.setEnabled(false);
+				stopButton.setEnabled(true);
+				
+				isRun=true;
+			} else if(eventSource == stopButton) {
+				startButton.setEnabled(true);
+				stopButton.setEnabled(false);
+
+				isRun=false;
+			}
+		}
+		
+	}
 }
 
 
---
+++ java3/src/xyz/itwill/swing/PenguinMoveApp.java
@@ -0,0 +1,53 @@
+package xyz.itwill.swing;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class PenguinMoveApp  extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	//프레임의 폭과 높이를 표현한 상수필드 선언
+	private static final int JFRAME_WIDTH=646;
+	private static final int JFRAME_HEIGHT=461;
+	
+	//배경이미지를 저장하기 위한 필드
+	//Image 클래스: 사진 파일(JPEG, GIF, PNG 파일 등)의 이미지 정보를 저장하기 위한 클래스
+	private Image backImage;
+	
+	
+	public PenguinMoveApp(String title) {
+		super(title);
+		
+		//ImageIcon 클래스: 사진파일에 대한 정보를 저장하기 위한 클래스
+		// =>ImageIcon 클래스의 ImageIcon(URL location)의 생성자 매개변수에
+		//사진파일의 URL 주소를 전달받아 ImageIcon 객체 생성
+		//URL: 리소스 파일(사진  파일, 소리 파일, 동영상 파일 등)의 파일 경로 또는
+		//인터넷 경로를 저장하기 위한 클래스
+		//Object.getClass(): 현재 실행중인 클래스의 Class 객체(메모리에 저장된 클래스)를 반환하는 메소드
+		//Class.getResource(String name): 현재 실행중인 클래스의 파일 경로를 기준으로 리소스
+		//파일을 읽어 리소스 파일의 경로가 저장된 URL 객체를 반환하는 메소드
+		//ImageIcon.getImage(): ImageIcon 객체에 저장된 사진파일에서 이미지가 저장된 Image 객체를 반환하는 메소드
+		backImage=new ImageIcon(getClass().getResource("/images/back.jpg")).getImage();
+		
+		setResizable(false);//프레임의 크기를 변경하지 않도록 설정
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		//setBounds(700, 200, 646, 461);
+		setBounds(700, 200, JFRAME_WIDTH, JFRAME_HEIGHT);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new PenguinMoveApp("펭귄 이동");
+	}
+	@Override
+	public void paint(Graphics g) {
+		//super.paint(g);
+		
+		//Graphics.drawImage(Image image, int x, int y, int width, int height, ImageObserver observer)
+		// => 이미지를 출력하는 메소드
+		g.drawImage(backImage, 0,0, JFRAME_WIDTH, JFRAME_HEIGHT, this);
+	}
+}
