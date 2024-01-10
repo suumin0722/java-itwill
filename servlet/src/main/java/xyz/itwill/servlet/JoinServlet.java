bf5e2597a7b4fae8bd3665f93d128a36ba4d8ea7
itwill <suumin0722@naver.com>
2024-01-10 20:34:19 +0900

    Day58

---
+++ servlet/src/main/java/xyz/itwill/servlet/JoinServlet.java
@@ -0,0 +1,133 @@
+package xyz.itwill.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//입력페이지(form.html)로부터 전달받은 입력값(회원정보)을 반환받아 클라이언트에게 HTML 문서를
+//전달하여 응답하는 서블릿
+// => 입력페이지의 form 태그를 사용하여 POST 방식으로 요청해야 실행되는 서블릿
+@WebServlet("/join.itwill")
+public class JoinServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out=response.getWriter();
+		
+		//서블릿을 GET 방식으로 요청한 경우 비정상적인 요청으로 클라이언트에게 에러코드(4xx or 5xx)를 전달하여 응답하거나
+		//에러페이지로 이동되도록 URL 주소를 전달하여 응답
+		//HttpServletRequest.getMethod() : 서블릿을 요청한 요청방식(GET or POST)을 반환하는 메소드
+		System.out.println("요청방식 = "+request.getMethod());
+		if(request.getMethod().equals("GET")) {//서블릿을 GET 방식으로 요청한 경우
+			/*
+			//HttpServletResponse.sendError(int sc) : 클라이언트에게 에러코드를 전달하여 응답하는 메소드
+			// => 상태코드(StatusCode)는 HttpServletResponse 인터페이스에 상수로 선언되어 제공
+			//[web.xml]파일에서 error-page 엘리먼트를 사용해 에러코드에 따른
+			//에러페이지를 전달하여 출력하는 기능 제공
+			// => 클라이언트의 요청 URL 주소는 변경되지 않고 에러페이지 출력
+			//response.sendError(400);
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+			//response.sendError(405);
+			//response.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+			return;//서블릿 종료
+			*/
+		
+			/*
+			//HttpServletResponse.sendRedirect(String url) : 클라이언트에게 301 상태코드와
+			//URL 주소를 전달하는 메소드
+			// => 301 상태코드를 전달받은 클라이언트는 응답받은 URL 주소로 브라우저의 요청
+			//URL 주소를 변경하여 재요청하여 실행결과를 응답받아 출력 - 페이지 이동(리다이렉트 이동)
+			response.sendRedirect("error.html");
+			return;
+			*/
+			
+			
+			
+			//클라이언트에서 HTML 문서에 자바스크립트를 저장하여 응답 처리
+			// => 경고창을 사용하여 메세지 출력 후 원하는 페이지로 이동 처리
+			out.println("<script>");
+			out.println("alert('비정상적인 방법으로 페이지를 요청하였습니다.');");
+			out.println("location.href='form.html';");
+			out.println("</script>");
+			return;
+		
+		}
+		
+		//서블릿을 POST 방식으로 요청한 경우 사용자 입력값을 리퀘스트 메세지의 몸체부에
+		//문서파일에 저장하여 전달
+		// => 리퀘스트 메세지의 몸체부에 만들어진 문서파일의 문자형태가 서유럽어(ISO-8859-1)로 설정되어 제공
+		// => 리퀘스트 메세지의 몸체부에 만들어진 문서파일의 문자형태를 변경한 후 전달값을 반환
+		//HttpServletRequest.setCharacterEncoding(String enclding) : 리퀘스트 메세지의 몸체부에 만들어진
+		//문서파일의 문자형태를 변경하는 메소드
+		// => GET 방식으로 요청한 경우 리퀘스트 메세지의 몸체부를 사용하지 않기 때문에 메소드 호출 불필요
+		request.setCharacterEncoding("utf-8");
+		
+		//서블릿을 요청할 때 전달된 값을 반환받아 저장
+		//HttpServletRequest.getParameter(String name) : 매개변수로 전달받은 이름의 전달값을
+		//문자열(String 객체)로 반환하는 메소드
+		// => 매개변수로 전달받은 이름의 전달값이 없는 경우 [null] 반환
+		String id=request.getParameter("id");
+		/*
+		//전달된 값에 대한 검증
+		if(id==null || id.equals("")) {//전달값이 없는 경우 - 비정상적인 요청
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+			return;
+		}
+			
+		if(!Pattern.matches("^[a-zA-Z]\\W{5,19}$", id)) {//전달값이 정규표현식의 패턴과 다른 경우 - 비정상적인 요청
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+			return;
+		}
+		*/
+		
+		String passwd=request.getParameter("passwd");
+		String name=request.getParameter("name");
+		String eamil=request.getParameter("eamil");
+		String gender=request.getParameter("gender");
+		//동일한 이름으로 전달되는 값이 여러개 있는 경우 첫번째 전달값만 반환받아 저장
+		//String hobby=request.getParameter("hobby");
+		//HttpServletRequest.getParameterValues(String name) : 매개변수로 전달받은 이름의 모든 전달값을
+		//문자열(String[])로 반환하는 메소드
+		String[] hobby=request.getParameterValues("hobby");
+		String job=request.getParameter("job"); 
+		String profile=request.getParameter("profile");
+		
+		//HTML 문서를 생성하여 클라이언트에게 전달하여 응답
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<meta charset='utf-8'>");
+		out.println("<title>Servlet</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>회원가입확인</h1>");
+		out.println("<hr>");
+		out.println("<p>아이디 ="+id+"</p>");
+		out.println("<p>비밀번호 ="+passwd+"</p>");
+		out.println("<p>이름 ="+name+"</p>");
+		out.println("<p>이메일 ="+eamil+"</p>");
+		out.println("<p>성별 ="+gender+"</p>");
+		//out.println("<p>취미 ="+hobby+"</p>");//그냥 hobby를 쓰면 배열의 주소가 출력됨
+		out.println("<p>취미 =");
+		for(int i=0;i<hobby.length;i++) {
+			out.println(hobby[i]);
+			if(i < hobby.length-1) {
+				out.println(",");
+			}
+		}
+		out.println("<p>직업 ="+job+"</p>");
+		//전달값의 엔터(Enter)를 <br> 태그로 변환해야만 줄바꿈 처리되어 출력
+		out.println("<p>자기소개 = <br>"+profile.replace("\n", "<br>")+"</p>");
+		out.println("</body>");
+		out.println("</html>");
+			
+	
+	}
+
+}
---
+++ servlet/src/main/java/xyz/itwill/servlet/LifeCycleServlet.java
@@ -0,0 +1,89 @@
+package xyz.itwill.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//서블릿은 클라이언트 요청에 의해 WAS 프로그램에 등록된 서블릿 클래스를 읽어 메모리에 저장하고
+//서블릿 객체를 생성하여 요청 처리 메소드 호출
+// => 클라이언트 요청에 의한 서블릿 객체가 이미 생성되어 있는 경우 새로운 서블릿 객체를 생성하지
+//않고 기존 서블릿 객체를 사용하여 요청 처리 메소드 호출
+// => WAS 프로그램이 종료되면 WAS 프로그램에 의해 생성된 서블릿 객체 소멸
+// => WAS 프로그램은 서블릿 객체를 관리하는 웹컨테이너(Web Container) 기능 제공
+
+//컨테이너(Container) : 프로그램에 실행에 필요한 객체의 생명주기(LifeCycle)를 관리하는 프로그램
+// => 서블릿은 컨테이너에 의해 관리되는 객체(Bean) - 컴퍼넌트(Component) : JavaBean
+
+@WebServlet("/life.itwill")
+public class LifeCycleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private String name;
+	
+	//서블릿 클래스를 객체로 생성하기 위한 생성자(Constructor)
+	// => 객체를 생성하면서 실행될 초기화 명령 작성 - 필드 초기화
+	// => 기본 생성자 안만들어도 됨. 알아서 처리함.
+	public LifeCycleServlet() {
+		System.out.println("### LifeCycleServlet 클래스의 기본 생성자 호출 - 객체 생성 ###");
+		//name="홍길동";
+	}
+	
+	
+	//서블릿 객체가 생성된 후 WAS 프로그램에 의해 자동으로 1번만 호출되는 메소드
+	// => 객체를 생성한 후 실행될 초기화 명령 작성 - 필드 초기화
+	//생성자 대신 init() 메소드를 사용하여 초기화 처리 명령을 작성하는 이유
+	// => init() 메소드는 매개변수로 ServletConfig 객체를 제공받아 사용 가능
+	//ServletConfig 객체 : 웹자원을 생성하기 위한 환경설정 관련 정보를 저장한 객체
+	// => [web.xml] 파일에서 제공되는 값을 얻어와 init() 메소드에서 사용 가능 - 유지보수의 효율성 증가 
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("### LifeCycleServlet 클래스의 init() 호출 - 객체 생성 ###");
+		//name="홍길동";
+		
+		//ServletConfig.getServletContext() : ServletContext 객체를 반환하는 메소드
+		// => ServletContext 객체 : 웹자원(WebContext)을 관리하기 위한 객체 - WAS 프로그램
+		//ServletContext.getInitParameter(String name) : [web.xml] 파일의 context-param 엘리먼트로
+		//제공되는 값을 매개변수로 전달된 이름으로 얻어와 반환하는 메소드
+		name=config.getServletContext().getInitParameter("name");
+	}
+	
+	//서블릿 객체가 소멸되기 전에 WAS 프로그램에 의해 자동으로 1번만 호출되는 메소드
+	// => WAS 프로그램이 종료되면 서블릿 객체는 WAS 프로그램에 의해 소멸 처리
+	// => 객체 소멸 전 실행될 마무리 명령 작성
+	// => 일반적으로 이 메소드는 만들지 않음! C언어의 경우 메모리 정리를 위해 사용하지만, 
+	//자바는 가비지 컬렉터가 자동으로 정리해주기 때문에 호출 하지 않아도 됨
+	@Override
+	public void destroy() {
+		System.out.println("### LifeCycleServlet 클래스의 destroy() 메소드 호출 ###");
+	}
+	
+	
+	//클라이언트가 서블릿을 요청할 때마다 WAS 프로그램에 의해 자동 호출되는 메소드 
+	// => 요청에 대한 처리 명령 작성 - 데이터 처리 및 응답 파일 생성 - 요청 처리 메소드
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//System.out.println("### LifeCycleServlet 클래스의 service() 메소드 호출 ###");
+	
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out=response.getWriter();
+		
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<meta charset='utf-8'>");
+		out.println("<title>Servlet</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>서블릿의 생명주기(Life Cycle)</h1>");
+		out.println("<hr>");
+		out.println("<p>"+name+"님 안녕하세요.</p>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+}
