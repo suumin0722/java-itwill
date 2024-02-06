16edd72da35958f2a78ef14fb467b466ded82c2d
itwill <suumin0722@naver.com>
2024-02-06 14:39:21 +0900

    Day77

---
+++ ajax/src/main/java/xyz/itwill/utility/Utility.java
@@ -0,0 +1,15 @@
+package xyz.itwill.utility;
+
+public class Utility {
+	//문자열을 전달받아 태그 관련 문자를 회피문자로 변경하여 반환하는 메소드
+	public static String escapeTag(String source) {
+		return source.replace("<", "&lt;").replace(">", "&gt;");
+	}
+	
+	//JSON 형식의 데이터에서 문자값으로 표현 불가능한 문자를 회피문자를 변환하여 반환하는 메소드
+	public static String toJSON(String source) {
+		return source.replace("\\", "\\\\").replace("\"", "\\\"")
+				.replace("\n", "\\n").replace("\r\n", "\\n");
+			
+	}
+}
