f2ca52c26cbbd67877ed1b01d2b862d404e098c2
itwill <suumin0722@naver.com>
2024-03-06 09:32:48 +0900

    Day88

---
+++ mybatis/src/main/java/xyz/itwill/dao/MyCommentDAO.java
@@ -0,0 +1,61 @@
+package xyz.itwill.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import xyz.itwill.dto.MyComment1;
+import xyz.itwill.dto.MyComment2;
+import xyz.itwill.mapper.MyCommentMapper;
+
+public class MyCommentDAO extends AbstractSession {
+	private static MyCommentDAO _dao;
+	
+	public MyCommentDAO() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	static {
+		_dao=new MyCommentDAO();
+	}
+	
+	public static MyCommentDAO getDAO() {
+		return _dao;
+	}
+	
+	public int insertComment1(MyComment1 comment1) {
+		SqlSession sqlSession=getSqlSessionFactory().openSession(true);
+		try {
+			return sqlSession.getMapper(MyCommentMapper.class).insertCopmment1(comment1);
+		} finally {
+			sqlSession.close();
+		}
+	}
+	
+	public int insertComment2(MyComment1 comment) {
+		SqlSession sqlSession=getSqlSessionFactory().openSession(true);
+		try {
+			return sqlSession.getMapper(MyCommentMapper.class).insertComment2(comment);
+		} finally {
+			sqlSession.close();
+		}
+	}
+	
+	public List<MyComment1> selectCommentList1() {
+		SqlSession sqlSession=getSqlSessionFactory().openSession(true);
+		try {
+			return sqlSession.getMapper(MyCommentMapper.class).selectCommentList1();
+		} finally {
+			sqlSession.close();
+		}
+	}
+	
+	public List<MyComment2> selectCommentList2() {
+		SqlSession sqlSession=getSqlSessionFactory().openSession(true);
+		try {
+			return sqlSession.getMapper(MyCommentMapper.class).selectCommentList2();
+		} finally {
+			sqlSession.close();
+		}
+	}
+}
