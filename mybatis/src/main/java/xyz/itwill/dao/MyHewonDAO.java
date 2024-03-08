a8e3ab076e6d5d5d5b992823ce47b1091e59ec84
itwill <suumin0722@naver.com>
2024-03-08 18:05:23 +0900

    Day91

--- mybatis/src/main/java/xyz/itwill/dao/MyHewonDAO.java
+++ mybatis/src/main/java/xyz/itwill/dao/MyHewonDAO.java
@@ -1,6 +1,7 @@
 package xyz.itwill.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 
@@ -44,6 +45,87 @@ public class MyHewonDAO extends AbstractSession {
 		SqlSession sqlSession=getSqlSessionFactory().openSession(true);
 		try {
 			return sqlSession.getMapper(MyHewonMapper.class).selectDiscriminatorHewonList();
+		} finally {
+			sqlSession.close();
+		}
+	}
+	
+	public List<MyHewon> selectStatusHewonList(int status) {
+		SqlSession sqlSession=getSqlSessionFactory().openSession(true);
+		try {
+			return sqlSession.getMapper(MyHewonMapper.class).selectStatusHewonList(status);
+		} finally {
+			sqlSession.close();
+		}
+	}
+	
+	public String selectDtoHewonId(MyHewon hewon) {
+		SqlSession sqlSession=getSqlSessionFactory().openSession(true);
+		try {
+			return sqlSession.getMapper(MyHewonMapper.class).selectDtoHewonId(hewon);
+		} finally {
+			sqlSession.close();
+		}
+	}
+	
+	public String selectMapHewonId(Map<String, Object> map) {
+		SqlSession sqlSession=getSqlSessionFactory().openSession(true);
+		try {
+			return sqlSession.getMapper(MyHewonMapper.class).selectMapHewonId(map);
+		} finally {
+			sqlSession.close();
+		}
+	}
+	
+	public int insertMapHewon(Map<String, Object> map) {
+		SqlSession sqlSession=getSqlSessionFactory().openSession(true);
+		try {
+			return sqlSession.getMapper(MyHewonMapper.class).insertMapHewon(map);
+		} finally {
+			sqlSession.close();
+		}
+	}
+	
+	public List<Map<String, Object>> selectMapHewonList() {
+		SqlSession sqlSession=getSqlSessionFactory().openSession(true);
+		try {
+			return sqlSession.getMapper(MyHewonMapper.class).selectMapHewonList();
+		} finally {
+			sqlSession.close();
+		}
+	}
+	
+	public String selectParamHewonId(String name, String email) {
+		SqlSession sqlSession=getSqlSessionFactory().openSession(true);
+		try {
+			return sqlSession.getMapper(MyHewonMapper.class).selectParamHewonId(name, email);
+		} finally {
+			sqlSession.close();
+		}
+	}
+	
+	public List<MyHewon> selectSearchHewonList(Map<String, Object> map) {
+		SqlSession sqlSession=getSqlSessionFactory().openSession(true);
+		try {
+			return sqlSession.getMapper(MyHewonMapper.class).selectSearchHewonList(map);
+		} finally {
+			sqlSession.close();
+		}
+	}
+	
+	public List<MyHewon> selectDynamicNameHewonList(String name) {
+		SqlSession sqlSession=getSqlSessionFactory().openSession(true);
+		try {
+			return sqlSession.getMapper(MyHewonMapper.class).selectDynamicNameHewonList(name);
+		} finally {
+			sqlSession.close();
+		}
+	}
+	
+	public List<MyHewon> selectDynamicIdNameHewonList(Map<String, Object> map) {
+		SqlSession sqlSession=getSqlSessionFactory().openSession(true);
+		try {
+			return sqlSession.getMapper(MyHewonMapper.class).selectDynamicIdNameHewonList(map);
 		} finally {
 			sqlSession.close();
 		}
